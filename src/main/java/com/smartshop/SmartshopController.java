caebf436c217718d994b177899976e4fde0ba181
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-09-04 22:21:51 +0700

    Add Smartshop functions

--- src/main/java/com/smartshop/SmartshopController.java
+++ src/main/java/com/smartshop/SmartshopController.java
@@ -1,21 +1,53 @@
 package com.smartshop;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class SmartshopController {
 
-    @RequestMapping("/")
-    public String index(){
-        return "Hello";
+    @RequestMapping("/product/all")
+    public List<Watch> index(){
+        return SmartshopStore.getAll();
+    }
+
+    @RequestMapping("/product/add")
+    public Map<String, Boolean> add(@RequestBody Watch watch){
+        if (SmartshopStore.add(watch)) {
+            HashMap<String, Boolean> response = new HashMap<>();
+            response.put("Success", true);
+            return response;
+        }
+        HashMap<String, Boolean> response = new HashMap<>();
+        response.put("Success", false);
+        return response;
+    }
+
+    @RequestMapping("/product/remove")
+    public Map<String, Boolean> remove(@RequestParam(value = "id") int id){
+        if(SmartshopStore.remove(id)){
+            HashMap<String, Boolean> response = new HashMap<>();
+            response.put("Success", true);
+            return response;
+        }
+        HashMap<String, Boolean> response = new HashMap<>();
+        response.put("Success", false);
+        return response;
     }
 
     public static void main(String[] args){
+        SmartshopStore.initData();
         SpringApplication.run(SmartshopController.class, args);
     }
 }
---
+++ src/main/java/com/smartshop/Watch.java
@@ -0,0 +1,67 @@
+package com.smartshop;
+
+public class Watch {
+    private long id;
+    private String productName;
+    private String brand;
+    private double price;
+    private String content;
+    private String imageURL;
+
+    public Watch(long id, String productName, String brand, double price, String content, String imageURL) {
+        this.id = id;
+        this.productName = productName;
+        this.brand = brand;
+        this.price = price;
+        this.content = content;
+        this.imageURL = imageURL;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
