217f75085a1a7779f8a4a4060b58d20df5a5a092
Phongsathorn Kittiworapanya <Phongsathron@outlook.com>
2019-09-04 18:25:16 +0700

    First init

---
+++ src/main/java/com/smartshop/SmartshopController.java
@@ -0,0 +1,21 @@
+package com.smartshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SmartshopController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello";
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(SmartshopController.class, args);
+    }
+}
